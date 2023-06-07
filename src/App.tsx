81517479e0678c0c2341c5a1ccdfd827305d2c03
Guilherme Silva Virgilli <gsvirgilli@gmail.com>
2023-06-07 18:48:10 -0300

    requisito 1°/2°/3°

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import './App.css';
+import { useState } from 'react';
+import Form from './components/Form';
+import Title from './components/Title';
 
 function App() {
+  const [cadastrar, setCadastrar] = useState(true);
+
+  const handleMostrarForm = () => {
+    setCadastrar(false);
+  };
+
+  const handleMostrarButton = () => {
+    setCadastrar(true);
+  };
+
   return (
     <div>
-      Hello World
+      <Title />
+      { cadastrar
+        ? <button onClick={ handleMostrarForm }>Cadastrar nova senha</button>
+        : <Form cancelar={ handleMostrarButton } />}
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,1 +1,30 @@
-Começando
+type CancelarProps = {
+  cancelar: (() => void)
+};
+
+function Form({ cancelar }:CancelarProps) {
+  return (
+    <>
+      <label htmlFor="name">
+        Nome do serviço
+        <input id="name" type="text" />
+      </label>
+      <label htmlFor="login">
+        Login
+        <input id="login" type="text" />
+      </label>
+      <label htmlFor="senha">
+        Senha
+        <input id="senha" type="password" />
+      </label>
+      <label htmlFor="url">
+        URL
+        <input id="url" type="text" />
+      </label>
+      <button disabled>Cadastrar</button>
+      <button onClick={ cancelar }>Cancelar</button>
+    </>
+  );
+}
+
+export default Form;
